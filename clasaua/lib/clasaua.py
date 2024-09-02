029dde1fc085de0709d114a3160d730f19ba01dd
Dani MF <dani@damufo.eu>
2024-09-02 08:15:40 +0200

    Move to version 0.4.10

--- clasaua/lib/clasaua.py
+++ clasaua/lib/clasaua.py
@@ -38,8 +38,8 @@ from clasaua.lib.clubs import clubs
 
 # ARGS = sys.argv[0] = re.sub(r'(-script\.py|-script\.pyw|\.exe)?$', '', sys.argv[0])
 # print(ARGS)
-APP_VERSION =  '0.4.9'
-APP_VERSION_DATE =  '2024-07-30'
+APP_VERSION =  '0.4.10'
+APP_VERSION_DATE =  '2024-09-02'
 
 
 EVENTS = (
